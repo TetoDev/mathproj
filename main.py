bbfb787305e86c848995081644024369b3b3f632
Octavio Massa <alonsomassa45@gmail.com>
2024-10-16 10:58:52 +0200

    Added quasi newton method, but nothing works yet haha

--- main.py
+++ main.py
@@ -15,6 +15,45 @@ def gradientLagrangien(r, h ,l ):
     z_component = (math.pi/3)*(r**2)*h - V0
     return [x_component, y_component, z_component]
 
+def hessianLagrangien(r, h, l):
+    # Second partial derivative with respect to r
+    d2f_dr2 = 1
+    # Second partial derivative with respect to h
+    d2f_dh2 = 1
+    # Second partial derivative with respect to l (constant term)
+    d2f_dl2 = 1
+    # Mixed partial derivative with respect to r and h
+    d2f_drh = 2
+    # Mixed partial derivative with respect to r and l
+    d2f_drl = 2
+    # Mixed partial derivative with respect to h and l
+    d2f_dhl = 5
+        
+    return [
+        [d2f_dr2, d2f_drh, d2f_drl],
+        [d2f_drh, d2f_dh2, d2f_dhl],
+        [d2f_drl, d2f_dhl, d2f_dl2]
+    ]
+
+def inverseMatrix(matrix):
+    inverse = [[matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1], matrix[0][2] * matrix[2][1] - matrix[0][1] * matrix[2][2], matrix[0][1] * matrix[1][2] - matrix[0][2] * matrix[1][1]],
+            [matrix[1][2] * matrix[2][0] - matrix[1][0] * matrix[2][2], matrix[0][0] * matrix[2][2] - matrix[0][2] * matrix[2][0], matrix[0][2] * matrix[1][0] - matrix[0][0] * matrix[1][2]],
+            [matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0], matrix[0][1] * matrix[2][0] - matrix[0][0] * matrix[2][1], matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]]]
+            
+    det = determinant(matrix)
+
+    for i in range(len(inverse)):
+        for j in range(len(inverse[i])):
+            inverse[i][j] = inverse[i][j] / det
+    
+    return inverse
+
+def determinant(matrix):
+    # Calculating the determinant of a 3x3 matrix
+    return (matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1]) -
+            matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0]) +
+            matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]))
+
 def fixedStepGradientMethod (r,h,l):
 
     X_k = [r,h,l]
@@ -54,6 +93,39 @@ def optimalStepGradientMethod(r,h,l):
         print (iterations,': ',dk)
         iterations += 1
     print(iterations)
+
+def newtonMethod(r, h, l):
+    pass
+
+def quasiNewtonMethod(r, h, l):
+    X_k = [r, h, l]
+    dk = [10, 10, 10]
+    iterations = 1
+    
+    while norm(dk) > 0.5 and iterations < 10000:
+
+        grad = gradientLagrangien(X_k[0], X_k[1], X_k[2])
+        hess = hessianLagrangien(X_k[0], X_k[1], X_k[2])
+        hess_inv = inverseMatrix(hess)
+            
+        dk = [
+            # I think this is wrong
+            hess_inv[0][0] * grad[0] + hess_inv[0][1] * grad[1] + hess_inv[0][2] * grad[2],
+            hess_inv[1][0] * grad[0] + hess_inv[1][1] * grad[1] + hess_inv[1][2] * grad[2],
+            hess_inv[2][0] * grad[0] + hess_inv[2][1] * grad[1] + hess_inv[2][2] * grad[2]
+        ]
+            
+        X_k = [
+            X_k[0] - dk[0],
+            X_k[1] - dk[1],
+            X_k[2] - dk[2]
+        ]
+            
+        print(iterations, ': ', dk)
+        iterations += 1
+    print(iterations)
+
+
     
 
 
@@ -62,7 +134,8 @@ def main ():
     r= 1 #random.random()*100
     h= 1 #random.random()*100
     l= -20 #random.random()*100
-    fixedStepGradientMethod(r, h, l)
-    optimalStepGradientMethod(r, h, l)
+    #fixedStepGradientMethod(r, h, l)
+    #optimalStepGradientMethod(r, h, l)
+    quasiNewtonMethod(r, h, l)
 
 main()
