8ce022165dfff357a2e877e6690c1f35c5f19e17
Octavio Massa <alonsomassa45@gmail.com>
2024-10-16 13:01:52 +0200

    Added Wolfe step NOT WORKING WE SUCK

--- main.py
+++ main.py
@@ -73,17 +73,82 @@ def fixedStepGradientMethod (r,h,l):
         iterations += 1
     print(iterations)
 
+def scalarProduct(v1,v2):
+    return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
+
+def wolfeStep(X_k, dk):
+
+    lagrange = lagrangien(X_k[0],X_k[1],X_k[1]) # X_k
+    gradient = gradientLagrangien(X_k[0],X_k[1],X_k[2]) # X_k
+
+    psk = scalarProduct(dk,gradient)
+
+    c1 = 0.1
+    c2 = 0.99
+
+    a_min = 0
+    a_max = 100
+    a = (a_min + a_max)/2
+
+    tooBig = True
+    tooSmall = True
+    iterations = 1 
+    while ((tooSmall or tooBig) and (iterations < 100)):
+        
+        #print(iterations,'. a: ', a)
+
+
+        X_k1 = [
+            X_k[0] + a*dk[0],
+            X_k[1] + a*dk[1],
+            X_k[2] + a*dk[2],
+        ]
+
+        lagrange_X_k1 = lagrangien(X_k1[0],X_k1[1],X_k1[2]) # X_k1
+        gradient_X_k1 = gradientLagrangien(X_k1[0],X_k1[1],X_k1[2]) # X_k1
+        psk1 = scalarProduct(gradient_X_k1,dk)
+
+        iterations += 1
+
+        print(lagrange_X_k1, ' > ', lagrange + c1*a*psk)
+        if (lagrange_X_k1 > (lagrange + c1*a*psk)):
+            tooBig = True
+
+            a_max = a
+
+            a= (a_min + a_max)/2
+        else:
+            tooBig = False
+
+        if (-psk1 > -c2*psk) :
+            tooSmall = True
+            
+            a_min = a
+
+            a= (a_min+a_max)/2
+        else :
+            tooSmall = False
+
+    
+    return a
+        
+        
+
+
 def optimalStepGradientMethod(r,h,l):
 
-    alpha = 0.001
-
+    alpha = 0
     X_k = [r,h,l]
-
     dk = [10,10,10]
+
+    
     iterations = 1
-    while (norm(dk) > 0.5 and iterations < 10000):
+    while (norm(dk) > 0.5 and iterations < 100):
 
         dk = gradientLagrangien(X_k[0], X_k[1], X_k[2])
+        alpha = wolfeStep(X_k,dk)
+        print('OptimalStep a: ', alpha)
+        
         
         X_k = [
             X_k[0] - alpha * dk[0],
@@ -135,7 +200,7 @@ def main ():
     h= 1 #random.random()*100
     l= -20 #random.random()*100
     #fixedStepGradientMethod(r, h, l)
-    #optimalStepGradientMethod(r, h, l)
-    quasiNewtonMethod(r, h, l)
+    optimalStepGradientMethod(r, h, l)
+    #quasiNewtonMethod(r, h, l)
 
 main()
